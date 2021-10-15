47d63f98177ca9dc151f1677616d26339e17ef33
patisoares <pati_soares75@hotmail.com>
2021-10-15 15:27:47 -0300

    alterando swagger

--- src/main/java/br/edu/ifms/ordemservico/dto/OrdemDeServicoDTO.java
+++ src/main/java/br/edu/ifms/ordemservico/dto/OrdemDeServicoDTO.java
@@ -3,7 +3,6 @@ package br.edu.ifms.ordemservico.dto;
 import java.io.Serializable;
 import java.time.Instant;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 
